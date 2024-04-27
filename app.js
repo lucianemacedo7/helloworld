c4d172b48521038c598f6eacad832836fb88bf81
Jakob Shavinski <shavinski.jakob@gmail.com>
2024-04-26 17:19:41 -0700

    create reusable variable for hostname

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const express = require("express");
 const app = express();
 
 const port = 3000;
-const hostname = "127.0.0.1";
+const hostname = "localhost";
 
 app.get("/", function (req, res) {
   res.send("Hello World!");
 });
 
 app.listen(port, function () {
-  console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server running at http://${hostname}:${port}`);
 });
