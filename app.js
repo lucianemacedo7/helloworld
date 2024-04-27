4109341049246ac6817f954bd2455fb3c0bcd8c0
Jakob Shavinski <shavinski.jakob@gmail.com>
2024-04-26 17:18:42 -0700

    Git rid of 127.0.0.1 and replace with localhost

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require("express");
-const square = require("./utils");
 const app = express();
+
 const port = 3000;
+const hostname = "127.0.0.1";
 
 app.get("/", function (req, res) {
   res.send("Hello World!");
-  console.log(`The area of a square with a width of 4 is ${square.area(4)}`);
 });
 
 app.listen(port, function () {
-  console.log(`Example app listening on port ${port}!`);
+  console.log(`Server running at http://localhost:${port}`);
 });
